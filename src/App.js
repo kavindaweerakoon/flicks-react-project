cdb0ab5129294df0e6ab59f878afcdfb5e32be53
Kavinda Weerakoon <kw@kwnetic.com>
2022-04-10 02:44:24 -0400

    4/10/2022

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Movies from "./pages/Movies";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Routes>
          
